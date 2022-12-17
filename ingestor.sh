65a3cde52eb1a09321d94a5416f57bf4ef5ddf1f
JesusCries <62108395+WesleyWong420@users.noreply.github.com>
2022-12-17 05:27:06 -0800

    Update ingestor.sh

--- ingestor.sh
+++ ingestor.sh
@@ -3,13 +3,10 @@ do
   sudo tcpdump -i eth0 -w tcpdump.pcap &
   sleep 3
   _pid=$! 
-  sleep 60
+  sleep 30
   kill -9 $_pid
   sleep 3
   data=`cat tcpdump.pcap`
-  curl -i -s -k -X $'POST' \
-      -H $'Host: localhost:3333' -H $'Accept: */*' -H $'Content-Type: multipart/form-data; boundary=---------------------------40903555511780393260267180294' -H $'Authorization: Basic cGlrYXJvb3Q6cGlrYXJvb3Q=' \
-      --data-binary $'\x0d\x0a-----------------------------40903555511780393260267180294\x0d\x0aContent-Disposition: form-data; name=\"file\"; filename=\"tcpdump.pcap\"\x0d\x0aContent-Type: application/vnd.tcpdump.pcap\x0d\x0a\x0d\x0a$data\x0d\x0a-----------------------------40903555511780393260267180294\x0d\x0aContent-Disposition: form-data; name=\"flush_all\"\x0d\x0a\x0d\x0afalse\x0d\x0a-----------------------------40903555511780393260267180294--\x0d\x0a' \
-      $'http://localhost:3333/api/pcap/upload'
+  python3 test.py
   sudo rm tcpdump.pcap
 done
